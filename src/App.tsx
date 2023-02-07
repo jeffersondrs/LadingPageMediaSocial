85d3d3101bba2458836bae83ebaf486ec0bebf4c
jeffersondrs <jeffersonduarte@id.uff.br>
2023-02-07 15:12:57 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,92 @@
+import "./App.css";
+import "./assets/AppStyles/App.scss";
+import "./assets/css/main.css";
+import { BsTwitter, BsWhatsapp, BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
+import { SiGmail } from "react-icons/si";
+
+function App() {
+  return (
+    <div className="App is-preload">
+      <div id="wrapper">
+        <div id="bg"></div>
+        {/* <div id="overlay"></div> */}
+        <div id="main">
+          <header id="header">
+            <h1 className="capitalize">Jefferson Santos</h1>
+            <p>
+              Desenvolvedor &nbsp;&bull;&nbsp; Full Stack &nbsp;&bull;&nbsp;
+              MERN
+            </p>
+            <nav>
+              <ul>
+                <li>
+                  <a
+                    href="https://twitter.com/kalliadranoth"
+                    className="icon brands"
+                  >
+                    <BsTwitter>
+                      <span className="label">Twitter</span>
+                    </BsTwitter>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.facebook.com/jefferson.duarteramos.1"
+                    className="icon brands"
+                  >
+                    <BsFacebook></BsFacebook>
+                    <span className="label">Facebook</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/jeffersondrs/"
+                    className="icon brands"
+                  >
+                    <BsLinkedin>
+                      <span className="label">Linkedin</span>
+                    </BsLinkedin>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://github.com/jeffersondrs"
+                    className="icon brands"
+                  >
+                    <BsGithub>
+                      <span className="label">Github</span>
+                    </BsGithub>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://gmail.com/" className="icon solid">
+                    <SiGmail>
+                      <span className="label">Email</span>
+                    </SiGmail>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://wa.me/+55021973152800" className="icon brands">
+                    <BsWhatsapp>
+                      <span className="label">Whatsapp</span>
+                    </BsWhatsapp>
+                    
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+
+          <footer className="bg-black/20 font-medium" id="footer">
+            <span className="copyright">
+              &copy; Lading Page Designed and edit by:{" "}
+              <a href="#">JEFFERSONDRS</a>.
+            </span>
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
